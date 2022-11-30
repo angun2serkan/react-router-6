88745e107cefdd31d329662e8e43e5e8a2b32692
Ethem Serkan Angün <serkan.angun@ecospend.com>
2022-11-30 16:21:02 +0300

    Navbar

--- src/App.js
+++ src/App.js
@@ -5,14 +5,17 @@ import Products from './pages/Products';
 import Error from './pages/Error';
 
 function App() {
-  return <BrowserRouter>
+  return (
+  <BrowserRouter>
+  <nav>our navbar</nav>
   <Routes>
     <Route path='/' element={<Home/>} />
     <Route path='about' element={<About/>} />
     <Route path='products' element={<Products/>} />
     <Route path='*' element={<Error/>} />
   </Routes>
-  </BrowserRouter>
+  <footer>our footer</footer>
+  </BrowserRouter>)
 }
 
 export default App;
