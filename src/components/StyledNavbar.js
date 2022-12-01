e889e9599492399b272824643f073f7364efeb10
Ethem Serkan Angün <serkan.angun@ecospend.com>
2022-12-01 16:24:50 +0300

    useNavigation

--- src/components/StyledNavbar.js
+++ src/components/StyledNavbar.js
@@ -8,6 +8,9 @@ function StyledNavbar() {
         <NavLink to='/'>Home</NavLink>
         <NavLink to='/about'>About</NavLink>
         <NavLink to='/products'>Products</NavLink>
+        <NavLink to='login' className={({isActive}) => (isActive ? 'link active' : 'link')}>
+          Login
+        </NavLink>
     </nav>
   )
 }
